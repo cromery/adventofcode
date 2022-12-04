756aad165c06acc0bb78f431bff8f38373b55e9a
cromery <christian.ek@choice.se>
2022-12-04 14:23:56 +0100

    day4

---
+++ 2022/day-4/index.js
@@ -0,0 +1,34 @@
+import * as helper from '../../helper.js'
+import { day4Input } from './day4-input.js'
+
+export const day4 = () => {
+    const assignmentList = helper.splitAtLineBreak(day4Input).map(pair => {
+        return pair.split(',').map(assignment => {
+            return assignment.split('-').map(string => {
+                return parseInt(string, 10)
+            });
+        })
+    });
+    let containedPairs = 0;
+    assignmentList.forEach(pair => {
+        const [firstLow, firstHigh] = pair[0];
+        const [secondLow, secondHigh] = pair[1];
+
+        if (firstLow <= secondLow && firstHigh >= secondHigh) {
+            containedPairs++;
+        } else if (secondLow <= firstLow && secondHigh >= firstHigh) {
+            containedPairs++;
+        }
+
+    });
+    console.log('containedPairs:', containedPairs);
+
+    let anyOverlapPairs = 0;
+    assignmentList.forEach(pair => {
+        const [firstLow, firstHigh] = pair[0];
+        const [secondLow, secondHigh] = pair[1];
+
+        if (firstHigh >= secondLow && firstLow <= secondHigh) anyOverlapPairs++;
+    });
+    console.log('anyOverlapPairs', anyOverlapPairs)
+};
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-import {day3} from './2022/day-3/index.js'
+import { day3 } from './2022/day-3/index.js'
+import { day4 } from './2022/day-4/index.js'
 
-day3();
+// day3();
+day4();
